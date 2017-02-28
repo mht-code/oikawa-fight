18bf27cc70dbf7686a55ea7dd224a6dd72b4c4ad
及川ひな <h.oikawa@mht-code.com>
2017-02-28 16:28:16 +0900

    TableViewを作成しました。

--- oikawa-fight/ViewController.swift
+++ oikawa-fight/ViewController.swift
@@ -8,19 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
     
-    //カラ館なう。 なう。
-    //アイスティー
+    // 5. テーブルに表示するテキスト
+    let texts = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "mac"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 4. delegateとdataSourceを設定
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    // 6. 必要なtableViewメソッド
+    // セルの行数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return texts.count
+    }
+    
+    // 6. 必要なtableViewメソッド
+    // セルのテキストを追加
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = texts[indexPath.row]
+        return cell
     }
 
 
