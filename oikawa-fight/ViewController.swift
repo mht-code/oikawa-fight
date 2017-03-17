7f60491e11efafb957f0a2ba1ef8fbc9bc098248
及川ひな <h.oikawa@mht-code.com>
2017-03-18 01:21:43 +0900

    わからないなりにいじったら余計にわからなくなってしまいました。

--- oikawa-fight/ViewController.swift
+++ oikawa-fight/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet var farst: UIView!
+    
+    
+
+    
 
 }
 
