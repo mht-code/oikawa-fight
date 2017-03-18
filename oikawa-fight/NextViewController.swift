d8984ee5b238347b8148778416afeea430fab05a
及川ひな <h.oikawa@mht-code.com>
2017-03-18 22:27:53 +0900

    主要メソッドを追加して理解することができました！

--- oikawa-fight/NextViewController.swift
+++ oikawa-fight/NextViewController.swift
@@ -1,36 +1,37 @@
 //
-//  ViewController.swift
+//  nextview.swift
 //  oikawa-fight
 //
-//  Created by 及川ひな on 2017/02/10.
+//  Created by 及川ひな on 2017/03/18.
 //  Copyright © 2017年 oikawa. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class NextViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("1個目のviewDidload")
+        print("2個目のviewDidload")
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("1WillAppear")
+        print("2WillAppear")
+        label.text = "taichi"
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("1DidAppear")
+        print("2DidAppear")
+        label.text = "hina"
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        print("1WillDisappear")
+        print("2WillDisappear")
     }
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        print("1DidDisappear")
+        print("2DidDisappear")
     }
-
-
 }
 
