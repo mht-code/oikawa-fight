110035e5b0644b7888934e765766553470abef80
及川ひな <h.oikawa@mht-code.com>
2017-03-23 12:00:00 +0900

    これらの手順を変更、追加しました。 キーボードをやっと隠せました。

--- oikawa-fight/ViewController.swift
+++ oikawa-fight/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var nametext: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        nametext.returnKeyType = .done
+
+        nametext.delegate = self
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        
+        
+        // キーボードを閉じる
+        textField.resignFirstResponder()
+        return true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+ 
+    
 
+   
 
+    @IBOutlet weak var namelabel: UILabel!
+    
+    @IBAction func namebutton(_ sender: Any) {
+        if nametext.text == "木村" {
+            namelabel.text = "拓也"
+        } else if nametext.text == "中居" {
+            namelabel.text = "正広"
+        } else if nametext.text == "稲垣" {
+            namelabel.text = "五郎"
+        } else if nametext.text == "草彅" {
+            namelabel.text = "剛"
+        } else if nametext.text == "香取"{
+            namelabel.text = "慎吾"
+        }
+    }
 }
+
 
