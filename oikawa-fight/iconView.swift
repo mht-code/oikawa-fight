8f2ec4221b6fed41388654d96238cc81fbc7a257
及川ひな <h.oikawa@mht-code.com>
2017-09-07 12:30:28 +0900

    [add] iconViewを追加

---
+++ oikawa-fight/iconView.swift
@@ -0,0 +1,15 @@
+//
+//  iconView.swift
+//  oikawa-fight
+//
+//  Created by 及川ひな on 2017/09/07.
+//  Copyright © 2017年 oikawa. All rights reserved.
+//
+
+import UIKit
+
+class iconView: UIView {
+
+    
+
+}
