7e2d4d65b55514ea2e0c6bb0a4d8d2ed69fb5887
及川ひな <h.oikawa@mht-code.com>
2017-09-07 21:32:52 +0900

    [add] カスタムビューのライフサイクルとメソッドを追加

--- oikawa-fight/View/iconView.swift
+++ oikawa-fight/View/iconView.swift
@@ -11,5 +11,19 @@ import UIKit
 class iconView: UIView {
 
     
+    override func layoutSubviews() {
+         super.layoutSubviews()
+
+        
+        let size = CGSize(width: 50, height: 50)
+        
+        let contentRect = CGRect(x: 0, y: 0, width: size.width * 4, height: size.height)
+        let contentView = UIView(frame: contentRect)
+        
+        let subContentView = UIView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        subContentView.backgroundColor = .green
+        contentView.addSubview(subContentView)
+        
+    }
 
 }
