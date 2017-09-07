f32e895c892f0481f457868957738a7b694eab1f
及川ひな <h.oikawa@mht-code.com>
2017-09-07 21:30:31 +0900

    [add] カスタムセルを作成

---
+++ oikawa-fight/Cell/iconCell.swift
@@ -0,0 +1,24 @@
+//
+//  iconCellTableViewCell.swift
+//  oikawa-fight
+//
+//  Created by 及川ひな on 2017/09/07.
+//  Copyright © 2017年 oikawa. All rights reserved.
+//
+
+import UIKit
+
+class iconCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
