117224ddcf7db38bb2645de26fb858ac755cda7a
及川ひな <h.oikawa@mht-code.com>
2017-09-08 18:30:46 +0900

    [add] カスタムビューをカスタムセルに表示

--- oikawa-fight/Cell/iconCell.swift
+++ oikawa-fight/Cell/iconCell.swift
@@ -11,17 +11,26 @@ import UIKit
 class iconCell: UITableViewCell {
     
     
-//    @IBOutlet weak var view: UIView!
-    
-    let view = UIView()
+    @IBOutlet weak var view: UIView!
     
 
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        let xibView: UIView = iconView(frame: CGRect(x: 0, y: 0, width: 300, height: 200))
-        view.addSubview(xibView)
+//        let xibView: UIView = iconView(frame: CGRect(x: 0, y: 0, width: 300, height: 200))
+//        view.addSubview(xibView)
+        
+        let size = CGSize(width: 50, height: 50)
+        
+        let contentRect = CGRect(x: 0, y: 0, width: size.width * 4, height: size.height)
+        let contentView = UIView(frame: contentRect)
+        
+        let subContentView: UIView = iconView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        subContentView.backgroundColor = .green
+        contentView.addSubview(subContentView)
         // このクラスは、キービューに対してキー値コーディングに準拠していません。
+        view.addSubview(contentView)
+    
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
