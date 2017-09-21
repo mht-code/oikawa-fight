5fc72d65be927ec1ce5341670ccba4962609652f
及川ひな <h.oikawa@mht-code.com>
2017-09-21 17:30:25 +0900

    [add] UIColor配列の値の数だけViewを表示させるメソッドを追加

--- oikawa-fight/Cell/IconCell.swift
+++ oikawa-fight/Cell/IconCell.swift
@@ -18,8 +18,34 @@ class IconCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        
+        viewGeneration()
+    }
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    func viewGeneration() {
+        for addView in 1 ... colors.count {
+            let size = CGSize(width: 80, height: 80)
+            
+            let contentRect = CGRect(x: 0, y: 0, width: size.width * 4, height: size.height)
+            let contentView = UIView(frame: contentRect)
+            
+            let subContentView: UIView = iconView(frame: CGRect(x: 116, y: 0, width: size.width, height: size.height))
+            subContentView.backgroundColor = .green
+            contentView.addSubview(subContentView)
+            // このクラスは、キービューに対してキー値コーディングに準拠していません。
+            view.addSubview(contentView)
+            colorsView()
+            print(addView)
+            
+        }
+    }
+    
+    func colorsView() {
         let size = CGSize(width: 80, height: 80)
         
         let contentRect = CGRect(x: 0, y: 0, width: size.width * 4, height: size.height)
@@ -31,12 +57,6 @@ class IconCell: UITableViewCell {
         // このクラスは、キービューに対してキー値コーディングに準拠していません。
         view.addSubview(contentView)
         
-    
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-    }
-    
 }
