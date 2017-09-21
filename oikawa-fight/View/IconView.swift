ec919d75344a78041d94fe5b5839e6517fea2add
行木太一 <r83if7zfj@gmail.com>
2017-09-22 00:54:42 +0900

    カスタムView読み込むロジック修正

--- oikawa-fight/View/IconView.swift
+++ oikawa-fight/View/IconView.swift
@@ -22,8 +22,20 @@ class IconView: UIView {
     
     
     func loadNib() {
-        let view = Bundle.main.loadNibNamed("IconView", owner: self, options: nil)?.first as! UIView
-        view.frame = self.bounds
-        self.addSubview(view)
+        // xib読み込み
+        let view = Bundle(for: type(of: self)).loadNibNamed("IconView", owner: self, options: nil)?.first as! UIView
+        addSubview(view)
+        // NSLayoutConstraint
+        view.translatesAutoresizingMaskIntoConstraints = false
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "H:|-0-[view]-0-|",
+            options: NSLayoutFormatOptions(rawValue: 0),
+            metrics: nil,
+            views: ["view" : view]))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat: "V:|-0-[view]-0-|",
+            options: NSLayoutFormatOptions(rawValue: 0),
+            metrics: nil,
+            views: ["view" : view]))
     }
 }
