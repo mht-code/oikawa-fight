b8df9412678a04d9468924d197c634c47f4ab9cb
及川ひな <h.oikawa@mht-code.com>
2017-09-23 18:41:01 +0900

    [add] 配列の値の個数だけViewの表示、x軸の追加、カラーを変更するfor文を追加

--- oikawa-fight/Cell/IconCell.swift
+++ oikawa-fight/Cell/IconCell.swift
@@ -10,10 +10,7 @@ import UIKit
 
 class IconCell: UITableViewCell {
     
-    
-//    @IBOutlet weak var view: UIView!
-    
-    let colors: [UIColor] = [UIColor.green, UIColor.blue, UIColor.red, UIColor.yellow]
+    let colors: [UIColor] = [UIColor.blue, UIColor.red, UIColor.yellow, UIColor.green]
 
 
     override func awakeFromNib() {
@@ -30,58 +27,78 @@ class IconCell: UITableViewCell {
     }
     
     func test() {
-        for i in colors { colors.index
+        
+        colors.enumerated().forEach() { index, color in
+            
             let margin = 15
             let size = 115
             
-            let x = margin + size
+            let x = (margin + size) * index
             
             
-            let testView = UIView.init(frame: CGRect.init(x: 15, y: 15, width: 100, height: 100))
-            let blue = UIColor.blue
-            testView.backgroundColor = blue
+            let testView = UIView.init(frame: CGRect.init(x: x, y: 15, width: 100, height: 100))
+            testView.backgroundColor = color
             self.contentView.addSubview(testView)
             
+            print(color)
+            print(index)
             
-            
-//            let secondView = UIView.init(frame: CGRect.init(x: x, y: 15, width: 100, height: 100))
-//            let green = UIColor.green
-//            secondView.backgroundColor = green
-//            self.contentView.addSubview(secondView)
-//            
-//            
-//            let thirdView = UIView.init(frame: CGRect.init(x: 0, y: 15, width: 100, height: 100))
-//            let red = UIColor.red
-//            thirdView.backgroundColor = red
-//            self.contentView.addSubview(thirdView)
-//            
-            
-            print(i)
         }
+        
+//        for i in colors { colors.index
+//            let margin = 15
+//            let size = 115
+//            
+//            let x = margin + size
+//            
+//            
+//            let testView = UIView.init(frame: CGRect.init(x: 15, y: 15, width: 100, height: 100))
+//            let blue = UIColor.blue
+//            testView.backgroundColor = blue
+//            self.contentView.addSubview(testView)
+//            
+//
+//            
+//            print(i)
+//        }
         
 
         
     }
     
     
-    func viewGeneration() {
-        for addView in 1 ... colors.count {
-            
-            let margin = 15
-            let size = 115
-            
-            let x = margin + size
-            
-            let testView = UIView.init(frame: CGRect.init(x: 15, y: 15, width: 100, height: 100))
-            let blue = UIColor.blue
-            testView.backgroundColor = blue
-            self.contentView.addSubview(testView)
-            
-            print(addView)
-            print(x)
-            
-        }
-    }
+//    func viewGeneration() {
+////        for addView in 1 ... colors.count {
+////            
+////            let margin = 15
+////            let size = 115
+////            
+////            let x = margin + size
+////            
+////            let testView = UIView.init(frame: CGRect.init(x: 15, y: 15, width: 100, height: 100))
+////            let blue = UIColor.blue
+////            testView.backgroundColor = blue
+////            self.contentView.addSubview(testView)
+////            
+////            print(addView)
+////            print(x)
+////            
+////            
+////            
+////            //            let secondView = UIView.init(frame: CGRect.init(x: x, y: 15, width: 100, height: 100))
+////            //            let green = UIColor.green
+////            //            secondView.backgroundColor = green
+////            //            self.contentView.addSubview(secondView)
+////            //
+////            //
+////            //            let thirdView = UIView.init(frame: CGRect.init(x: 0, y: 15, width: 100, height: 100))
+////            //            let red = UIColor.red
+////            //            thirdView.backgroundColor = red
+////            //            self.contentView.addSubview(thirdView)
+////            //
+////            
+////        }
+//    }
     
 
 }
