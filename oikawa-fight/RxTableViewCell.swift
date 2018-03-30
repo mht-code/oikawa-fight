f22ac2f7f6a0561adbb281c7fad4607e91203be6
及川ひな <h.oikawa@mht-code.com>
2018-03-30 23:36:57 +0900

    TableViewのサンプルアプリを実装

---
+++ oikawa-fight/RxTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RxTableViewCell.swift
+//  oikawa-fight
+//
+//  Created by 及川ひな on 2018/03/30.
+//  Copyright © 2018年 oikawa. All rights reserved.
+//
+
+import UIKit
+
+class RxTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- oikawa-fight/ViewController.swift
+++ oikawa-fight/ViewController.swift
@@ -7,19 +7,67 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    var disposeBag: DisposeBag = DisposeBag()
+
+    let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, String>>(configureCell: {(_, _, _, _ ) in
+        fatalError()
+    })
+    let viewModel: ViewModel = ViewModel()
+
+    var cellItems: Variable<[String]> = Variable([])
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.tableFooterView = UIView()
+        tableView.backgroundColor = UIColor.lightGray
+
+        let nib = UINib(nibName: "RxTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "RxTableViewCell")
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
+        //elementはSectionModelの対応するitem
+        dataSource.configureCell = { (_, table: UITableView, indexPath: IndexPath, element: String) in
+            let cell = table.dequeueReusableCell(withIdentifier: "RxTableViewCell") as! RxTableViewCell
+            cell.textLabel?.value(forKey: element)
+            return cell
+        }
+
+        //sectionのタイトルを設定する
+        dataSource.titleForHeaderInSection = { dataSource, sectionIndex in
+            return dataSource[sectionIndex].model
+        }
+
+        viewModel.observableItems.bind(to: tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+
+        tableView.rx
+            .setDelegate(self)
+            .disposed(by: disposeBag)
+
+    }
+
+    // to prevent swipe to delete behavior
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .none
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
