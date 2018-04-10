4ba1213c01160328b89ec7c584fa5f44f062e173
及川ひな <h.oikawa@mht-code.com>
2018-04-10 22:09:10 +0900

    UnitTestを追加

---
+++ oikawa-fight/BusinessModel.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessModel.swift
+//  oikawa-fight
+//
+//  Created by 及川ひな on 2018/04/10.
+//  Copyright © 2018年 oikawa. All rights reserved.
+//
+
+import Foundation
+
+public struct BusinessModel {
+
+    let memberId: String
+    let message: String
+    let count: Int
+
+}
---
+++ oikawa-fightTests/oikawa_fightTests.swift
@@ -0,0 +1,32 @@
+//
+//  oikawa_fightTests.swift
+//  oikawa-fightTests
+//
+//  Created by 及川ひな on 2018/04/10.
+//  Copyright © 2018年 oikawa. All rights reserved.
+//
+
+import XCTest
+@testable import oikawa_fight
+
+class oikawa_fightTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testGenerateBusinessModel() {
+        let mapper = ModelMapper()
+        let entity = EntityData()
+        let bm = mapper.generateBusinessModel(entity: entity)
+
+        XCTAssertEqual(bm.memberId, "memberId")
+        XCTAssertEqual(bm.message, "message")
+        XCTAssertEqual(bm.count, 5)
+    }
+}
